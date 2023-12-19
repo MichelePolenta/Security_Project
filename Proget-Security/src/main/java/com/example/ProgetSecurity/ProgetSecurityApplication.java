8f673dda7461098897ef758a6bc064e2ac1dacd6
MichelePolenta <94616706+MichelePolenta@users.noreply.github.com>
2023-12-19 16:28:17 +0100

    Add files via upload

---
+++ Proget-Security/src/main/java/com/example/ProgetSecurity/ProgetSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgetSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgetSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgetSecurityApplication.class, args);
+	}
+
+}
---
+++ Proget-Security/src/test/java/com/example/ProgetSecurity/ProgetSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgetSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgetSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
